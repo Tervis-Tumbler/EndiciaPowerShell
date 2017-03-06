991867710e2b9381032e3b806f35996ab93f1ecd
Chris Magnuson <hourglasssand@gmail.com>
2017-03-06 11:25:06 -0500

    Initial commit

---
+++ EndiciaPowerShell.psm1
@@ -0,0 +1,81 @@
+﻿function Get-AccountStatus{
+    param(
+        $ResponseVersion,
+        $RequesterID,
+        $RequestID,
+        $AccountID,
+        $PassPhrase
+    )
+
+    $xmlstring = @"
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <GetAccountStatus xmlns="www.envmgr.com/LabelService">
+      <AccountStatusRequest ResponseVersion="$ResponseVersion">
+        <RequesterID>$RequesterID</RequesterID>
+        <RequestID>$RequestID</RequestID>
+        <CertifiedIntermediary>
+          <AccountID>$AccountID</AccountID>
+          <PassPhrase>$PassPhrase</PassPhrase>
+        </CertifiedIntermediary>
+      </AccountStatusRequest>
+    </GetAccountStatus>
+  </soap:Body>
+</soap:Envelope>
+"@
+    Invoke-WebRequest -Uri https://labelserver.endicia.com/LabelService/EwsLabelService.asmx -Method Post -ContentType "text/xml; charset=utf-8" -Body $xmlstring -Headers @{SOAPAction = "www.envmgr.com/LabelService/GetAccountStatus"}
+}
+
+function Get-ChallengeQuestion{
+    param(
+        $RequesterID,
+        $RequestID,
+        $AccountID,
+        $EMail
+    )
+
+    $xmlstring = @"
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <GetChallengeQuestion xmlns="www.envmgr.com/LabelService">
+      <ChallengeQuestionRequest>
+        <RequesterID>$RequesterID</RequesterID>
+        <RequestID>$RequestID</RequestID>
+        <AccountID>$AccountID</AccountID>
+        <EMail>$EMail</EMail>
+      </ChallengeQuestionRequest>
+    </GetChallengeQuestion>
+  </soap:Body>
+</soap:Envelope>
+"@
+    Invoke-WebRequest -Uri https://labelserver.endicia.com/LabelService/EwsLabelService.asmx -Method Post -ContentType "text/xml; charset=utf-8" -Body $xmlstring -Headers @{SOAPAction = "www.envmgr.com/LabelService/GetChallengeQuestion"}
+}
+
+function Reset-SuspendedAccountRequestXML{
+    param(
+        $RequesterID,
+        $RequestID,
+        $AccountID,
+        $ChallengeAnswer,
+        $NewPassPhrase
+    )
+$xmlstring = @"
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <ResetSuspendedAccount xmlns="www.envmgr.com/LabelService">
+      <ResetSuspendedAccountRequest>
+        <RequesterID>$RequesterID</RequesterID>
+        <RequestID>$RequestID</RequestID>
+        <AccountID>$AccountID</AccountID>
+        <ChallengeAnswer>$ChallengeAnswer</ChallengeAnswer>
+        <NewPassPhrase>$NewPassPhrase</NewPassPhrase>
+      </ResetSuspendedAccountRequest>
+    </ResetSuspendedAccount>
+  </soap:Body>
+</soap:Envelope>
+"@
+    Invoke-WebRequest -Uri https://labelserver.endicia.com/LabelService/EwsLabelService.asmx -Method Post -ContentType "text/xml; charset=utf-8" -Body $xmlstring -Headers @{SOAPAction = "www.envmgr.com/LabelService/ResetSuspendedAccount"}
+}
